38ff281154199c19540f34d60e7b6ed0a5813357
ArshamEbr <ebrarsham1@gmail.com>
2025-03-13 19:39:33 +0330

    restoring system suspend and some fixes

--- hyprland/hypridle.nix
+++ hyprland/hypridle.nix
@@ -6,36 +6,41 @@
 
    hypridle.settings = {
 
-     "$lock_cmd" = "pidof hyprlock || hyprlock";
-     "$suspend_cmd" = "pidof steam || systemctl suspend || loginctl suspend"; # fuck nvidia
-     
-     general = {
-       lock_cmd = "$lock_cmd";
-       before_sleep_cmd = "loginctl lock-session";
-     };
-     
-     listener = [
-       {
-         timeout = 180; # 3mins
-         on-timeout = "brightnessctl | grep 'Current' | awk '{ print $3 }' > ~/.cache/idle-brightness && brightnessctl set 1%";
-         on-resume = "brightnessctl set $(cat ~/.cache/idle-brightness)";
-       }
+    "$lock_cmd" = "pidof hyprlock || hyprlock";
+    "$suspend_cmd" = "pidof steam || pidof looking-glass-client || systemctl suspend || loginctl suspend"; # fuck nvidia
+    "$dpms_state_file" = "/tmp/dpms_off";
+    
+    general = {
+      lock_cmd = "$lock_cmd";
+      before_sleep_cmd = "loginctl lock-session";
+    };
+    
+    listener = [
+      {
+        timeout = 180; # 3mins
+        on-timeout = "brightnessctl | grep 'Current' | awk '{ print $3 }' > ~/.cache/idle-brightness && brightnessctl set 1%";
+        on-resume = "brightnessctl set $(cat ~/.cache/idle-brightness)";
+      }
 
-       {
-         timeout = 420; # 7mins
-         on-timeout = "$lock_cmd";
-       }
-
-       {
-         timeout = 600; # 10mins
-         on-timeout = "hyprctl dispatch dpms off";
-         on-resume = "hyprctl dispatch dpms on";
-       }
-
-     #  {
-     #    timeout = 540 # 9mins
-     #    on-timeout = $suspend_cmd
-     #  }
+      {
+        timeout = 420; # 7mins
+        on-timeout = "$lock_cmd";
+      }
+   
+      {
+        timeout = 600; # 10 mins
+         on-timeout = ''
+           [ -f "$dpms_state_file" ] || { hyprctl dispatch dpms off && touch "$dpms_state_file"; }
+         '';
+         on-resume = ''
+           [ ! -f "$dpms_state_file" ] || { hyprctl dispatch dpms on && rm -f "$dpms_state_file"; }
+         '';
+      }
+   
+    {
+      timeout = 7200; # 2 hours
+      on-timeout = "$suspend_cmd";
+    }
      
      ];
    };
