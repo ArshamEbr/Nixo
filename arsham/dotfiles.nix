6d9da2f61d44aca7adf48853c2151b1ab90645cb
ArshamEbr <ebrarsham1@gmail.com>
2025-04-19 08:28:38 +0330

    merging dot-files with pure nix

---
+++ arsham/dotfiles.nix
@@ -0,0 +1,52 @@
+# dotfiles.nix
+{ lib, config, ... }:
+
+let
+  inherit (lib) types mkOption mapAttrsToList replaceStrings concatStringsSep;
+  cfg = config.dotfiles;
+in
+{
+  options.dotfiles = {
+    username = mkOption {
+      type = types.str;
+      description = "Username for dotfiles";
+    };
+    files = mkOption {
+      type = types.attrsOf (types.submodule {
+        options = {
+          text = mkOption {
+            type = types.nullOr types.str;
+            default = null;
+          };
+          source = mkOption {
+            type = types.nullOr types.path;
+            default = null;
+          };
+        };
+      });
+      default = {};
+    };
+  };
+
+  config = lib.mkIf (cfg != {}) {
+    environment.etc = lib.mkMerge (
+      mapAttrsToList (target: val: {
+        "dotfiles/${replaceStrings ["/"] ["__"] target}" = 
+          if val.text != null then { text = val.text; }
+          else { source = val.source; };
+      }) cfg.files
+    );
+
+    system.activationScripts.linkDotfiles = {
+      text = ''
+        set -e
+        echo "Linking dotfiles..."
+        ${concatStringsSep "\n" (mapAttrsToList (target: val: ''
+          mkdir -p "/home/${cfg.username}/$(dirname "${target}")"
+          ln -sf "/etc/dotfiles/${replaceStrings ["/"] ["__"] target}" \
+            "/home/${cfg.username}/${target}"
+        '') cfg.files)}
+      '';
+    };
+  };
+}
