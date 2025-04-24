d2df39394996c9493930651e91b29e290318c77d
ArshamEbr <ebrarsham1@gmail.com>
2025-04-24 18:41:51 +0330

    merging btop

---
+++ dotfiles/btop/btop.nix
@@ -0,0 +1,17 @@
+{ pkgs, config, lib, user, pkgs-unstable, ... }:{
+
+  dotfiles = {
+      username = "${user.name}";
+      files = {
+      ".config/btop/btop.conf".text = ''
+        color_theme = "Default"
+        rounded_corners = True
+        theme_background = False
+        update_ms = 100
+      '';
+      };
+    };
+    environment.systemPackages = with pkgs-unstable; [
+      btop
+    ];
+  }
