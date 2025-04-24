6ccb07456588bf9aa4e0e1b6b95c004c8a1c5422
ArshamEbr <ebrarsham1@gmail.com>
2025-04-24 15:19:33 +0330

    cleaning

---
+++ arsham/default.nix
@@ -0,0 +1,15 @@
+{
+  imports = [
+    ./bash.nix
+    ./boot.nix
+    ./dotfiles.nix
+    ./games.nix
+    ./graphics.nix
+    ./networking.nix
+    ./power.nix
+    ./sfx.nix
+    ./warn.nix
+    ./virtualisation.nix
+    ./uki.nix
+  ];
+}
