a6d4848541d54e31060fc16dfb3bef6713661e02
ArshamEbr <ebrarsham1@gmail.com>
2025-04-28 12:35:35 +0330

    removing useless stuff

--- overlays/cider.nix
+++
@@ -1,8 +0,0 @@
-final: prev: {
-  cider = prev.cider.overrideAttrs (oldAttrs: {
-    postInstall = (oldAttrs.postInstall or "") + ''
-      substituteInPlace $out/share/applications/cider.desktop \
-        --replace "cider --no-sandbox %U" "env -u NIXOS_OZONE_WL cider --use-gl=desktop %U"
-    '';
-  });
-}
