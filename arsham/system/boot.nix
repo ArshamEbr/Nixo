6926fc4550045fbc79b0f0d05dad44b887e94411
ArshamEbr <ebrarsham1@gmail.com>
2025-07-15 21:57:01 +0330

    moving to latest xanmod kernel, some typo

--- arsham/system/boot.nix
+++ arsham/system/boot.nix
@@ -1,18 +1,18 @@
 {
-  pkgs, 
-  pkgs-stable, 
-  ... 
+  pkgs,
+  pkgs-stable,
+  ...
 }:
 
 {
   boot = {
-    extraModulePackages = [ pkgs-stable.linuxKernel.packages.linux_6_14.kvmfr ];
     extraModprobeConfig = "options snd_hda_intel model=alcplugfix";
     consoleLogLevel = 0;
     supportedFilesystems = [ "ntfs" "nfs" ];
-  #  kernelPackages = pkgs-unstable.linuxPackages_latest; # 6.13 kernel not fixed rn in jan 21
-  #  kernelPackages = pkgs-stable.linux_xanmod_latest;
-    kernelPackages = pkgs-stable.linuxPackages_6_14;
+    kernelPackages = pkgs.linuxPackages_xanmod_latest;
+    extraModulePackages = [
+      pkgs.linuxKernel.packages.linux_xanmod_latest.kvmfr
+    ];
     
     loader = {
       efi.canTouchEfiVariables = true;
@@ -22,56 +22,55 @@
     };
     
     kernelParams = [
-      "quiet"
-      "initcall_debug=n"
-      "systemd.show_status=0"
-      "fastboot"
-      "intel_iommu=on"          
-      "iommu=pt"                
-      "vfio-pci.ids=10de:1c94"
-      "vfio-pci.enable_msi=1"
-      "kvmfr_static_size_mb=64"
-      "i915.enable_psr=1"
+      "quiet"                    # Suppress most boot messages
+      "initcall_debug=n"         # Disable initcall debugging
+      "systemd.show_status=0"    # Hide systemd status messages
+      "fastboot"                 # Skip certain boot checks for faster boot
+      "intel_iommu=on"           # Enable Intel IOMMU for device passthrough
+      "iommu=pt"                 # Enable passthrough mode for IOMMU
+      "vfio-pci.ids=10de:1c94"   # Bind specific PCI device to vfio-pci
+      "vfio-pci.enable_msi=1"    # Enable MSI for vfio-pci devices
+      "kvmfr_static_size_mb=64"  # Set static memory size for kvmfr
+      "i915.enable_psr=1"        # Enable Panel Self Refresh for Intel graphics
     ];
     
     initrd = {
+      systemd.enable = true;
       verbose = false;
       compressor = "zstd";
-      systemd.enable = true;
       compressorArgs = [ 
         "-T0"
       ];
-        
-      availableKernelModules = [ 
+      
+      availableKernelModules = [
         "xhci_pci"
         "vmd"
         "ahci"
         "usb_storage"
         "sd_mod"
       ];
-        
+      
       kernelModules = [
-        "vfio_pci"          
+        "vfio_pci"
         "vfio"
         "vfio_iommu_type1"
-        "kvmfr"
       ];
     };
-      
+    
     plymouth = {
       enable = true;
       theme = "proxzima"; # rog_2
-      themePackages = with pkgs-stable; [
+      themePackages = with pkgs; [
         plymouth-matrix-theme
         plymouth-proxzima-theme
         adi1090x-plymouth-themes
       ];
     };
-    
+
     kernelModules = [
-        "i915"
-        "uinput"
-        "kvm-intel"
+      "i915"
+      "uinput"
+      "kvm-intel"
     ];
   };
 }
