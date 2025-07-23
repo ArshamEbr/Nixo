f5a0f1b36b12ec69feed6dc821874b37ff6c2a12
ArshamEbr <ebrarsham1@gmail.com>
2025-07-23 23:52:29 +0330

    simplify and clarify logic

--- arsham/system/dm/default.nix
+++ arsham/system/dm/default.nix
@@ -6,58 +6,61 @@ Fetches wallpapers by URL and hash, applies them as backgrounds, and enables SDD
 {
   pkgs,
   pkgs-stable,
-  inputs, 
+  inputs,
   ...
 }:
 
 let
-  mitsu-bg = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/ArshamEbr/nixo/stable/resources/wallpapers/mitsu.mp4";
-    hash = "sha256-wVGzVvvb3uPtY/iBtyvUjzKCqLBq6mLeXK+KIcQubNQ=";
-  };
-  l-from-death-note-bg = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/ArshamEbr/Nixo/test/resources/wallpapers/l-from-death-note.mp4";
-    hash = "sha256-FHp1X490lBlY4Y2wzsuh13U4Re7NmL76ui71NRWr8LY=";
-  };
-  anime-girl-rain-bg = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/ArshamEbr/Nixo/test/resources/wallpapers/anime-girl-rain.mp4";
-    hash = "sha256-nGgRRMa3Z4ivpTO829BnfvpkEeexTWS6tbW20TRyubA=";
-  };
-  beach-with-palm-trees-bg = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/ArshamEbr/Nixo/test/resources/wallpapers/beach-with-palm-trees.mp4";
-    hash = "sha256-eLyaGm+JKL0gzyxI56Co2kMpAOWqFQtkdywuL1E0iYc=";
-  };
-  orange-train-at-sunset-bg = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/ArshamEbr/Nixo/test/resources/wallpapers/orange-train-at-sunset.mp4";
-    hash = "sha256-plM55EKxEagVyBGXSQAb1fpxHO83yDmoJEkTzXsMTQE=";
-  };
-  waiting-snow-bg = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/ArshamEbr/Nixo/test/resources/wallpapers/waiting-snow.gif";
-    hash = "sha256-ykQ3rf1xMGGFCk37xO4BkMZV+ZEOgAODQm6NqbEev3Q=";
-  };
+  wall.name = "orange-train-at-sunset";
 in
-let 
+
+let
+  wallpapers = {
+    mitsu = {
+      name = "mitsu.mp4";
+      url = "https://raw.githubusercontent.com/ArshamEbr/nixo/stable/resources/wallpapers/mitsu.mp4";
+      hash = "sha256-wVGzVvvb3uPtY/iBtyvUjzKCqLBq6mLeXK+KIcQubNQ=";
+    };
+    l-from-death-note = {
+      name = "l-from-death-note.mp4";
+      url = "https://raw.githubusercontent.com/ArshamEbr/Nixo/test/resources/wallpapers/l-from-death-note.mp4";
+      hash = "sha256-FHp1X490lBlY4Y2wzsuh13U4Re7NmL76ui71NRWr8LY=";
+    };
+    anime-girl-rain = {
+      name = "anime-girl-rain.mp4";
+      url = "https://raw.githubusercontent.com/ArshamEbr/Nixo/test/resources/wallpapers/anime-girl-rain.mp4";
+      hash = "sha256-nGgRRMa3Z4ivpTO829BnfvpkEeexTWS6tbW20TRyubA=";
+    };
+    beach-with-palm-trees = {
+      name = "beach-with-palm-trees.mp4";
+      url = "https://raw.githubusercontent.com/ArshamEbr/Nixo/test/resources/wallpapers/beach-with-palm-trees.mp4";
+      hash = "sha256-eLyaGm+JKL0gzyxI56Co2kMpAOWqFQtkdywuL1E0iYc=";
+    };
+    orange-train-at-sunset = {
+      name = "orange-train-at-sunset.mp4";
+      url = "https://raw.githubusercontent.com/ArshamEbr/Nixo/test/resources/wallpapers/orange-train-at-sunset.mp4";
+      hash = "sha256-plM55EKxEagVyBGXSQAb1fpxHO83yDmoJEkTzXsMTQE=";
+    };
+    waiting-snow = {
+      name = "waiting-snow.gif";
+      url = "https://raw.githubusercontent.com/ArshamEbr/Nixo/test/resources/wallpapers/waiting-snow.gif";
+      hash = "sha256-ykQ3rf1xMGGFCk37xO4BkMZV+ZEOgAODQm6NqbEev3Q=";
+    };
+  };
+  
+  fetchedWallpapers = pkgs.lib.mapAttrs (_: wp: pkgs.fetchurl wp) wallpapers;
+  
   sddm-theme = inputs.silentSDDM.packages.${pkgs.system}.default.override {
     theme = "rei"; # silvia, ken, rei
-    extraBackgrounds = [
-      mitsu-bg 
-      l-from-death-note-bg 
-      anime-girl-rain-bg 
-      beach-with-palm-trees-bg 
-      orange-train-at-sunset-bg 
-      waiting-snow-bg
-    ];
+    extraBackgrounds = pkgs.lib.attrValues fetchedWallpapers;
     theme-overrides = {
-      "LoginScreen" = {
-        background = "orange-train-at-sunset.mp4";
-      };
-      "LockScreen" = {
-        background = "orange-train-at-sunset.mp4";
-      };
+      "LoginScreen".background = fetchedWallpapers.${wall.name}.name;
+      "LockScreen".background = fetchedWallpapers.${wall.name}.name;
     };
-  }; in  
+  };
+in
 {
-  environment.systemPackages = [sddm-theme];
+  environment.systemPackages = [ sddm-theme ];
   qt.enable = true;
   services.displayManager.sddm = {
     package = pkgs-stable.kdePackages.sddm;
@@ -65,11 +68,9 @@ let
     wayland.enable = true;
     theme = sddm-theme.pname;
     extraPackages = sddm-theme.propagatedBuildInputs;
-    settings = {
-      General = {
-        GreeterEnvironment = "QML2_IMPORT_PATH=${sddm-theme}/share/sddm/themes/${sddm-theme.pname}/components/,QT_IM_MODULE=qtvirtualkeyboard";
-        InputMethod = "qtvirtualkeyboard";
-      };
+    settings.General = {
+      GreeterEnvironment = "QML2_IMPORT_PATH=${sddm-theme}/share/sddm/themes/${sddm-theme.pname}/components/,QT_IM_MODULE=qtvirtualkeyboard";
+      InputMethod = "qtvirtualkeyboard";
     };
   };
 }
