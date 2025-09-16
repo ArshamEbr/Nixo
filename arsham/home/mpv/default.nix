7bc68d53b377134a1527aff5b131989dace55a85
ArshamEbr <ebrarsham1@gmail.com>
2025-09-16 17:45:43 +0330

    adding visualizer for mpv

--- arsham/home/mpv/default.nix
+++ arsham/home/mpv/default.nix
@@ -1,8 +1,35 @@
 {
+  pkgs,
+  ...
+}:
+
+{
   programs.mpv = {
     enable = true;
+    scripts = with pkgs.mpvScripts; [
+      mpris
+      sponsorblock
+      thumbfast
+      quality-menu
+    ];
+
     config = {
+      cache = "yes";
+      osc = "yes";
       keep-open = true;
     };
+
+    profiles = {
+      music = {
+        "audio-display" = "no";
+        "keep-open" = "yes";
+        "term-playing-msg" = "▶ /$/{filename}/";
+      };
+      video = {
+        "vo" = "gpu-next";
+        "hwdec" = "auto-safe";
+      };
+    };
+    home.file.".config/mpv/scripts/visualizer.lua".source = ../../../resources/scripts/visualizer.lua;
   };
 }
